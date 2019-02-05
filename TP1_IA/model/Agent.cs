f457071f514845936b2ce17ce61bdd204f82866f
ThDENIS <thibault.sined@gmail.com>
2019-02-04 22:32:14 -0500

    init des valeurs pour les mvmt + tentative de début d'agent pensant

--- TP1_IA/model/Agent.cs
+++ TP1_IA/model/Agent.cs
@@ -15,6 +15,7 @@ namespace TP1_IA.model
         private Desire _desire;
         private Intentions _intentions;
         private Capteurs _capteurs;
+        private Dictionary<String, int> _valeur;
 
         public Agent() : this(new Coordonnees(0,0), new Environnement()) { }
 
@@ -27,6 +28,10 @@ namespace TP1_IA.model
             _desire = new Desire();
             _intentions = new Intentions();
             _capteurs = new Capteurs();
+            _valeur = new Dictionary<string, int>();
+            _valeur.Add("deplacement", -1);
+            _valeur.Add("poussiere", 10);
+            _valeur.Add("bijou", 15);
         }
 
         public int distance(Coordonnees a, Coordonnees b)
@@ -37,12 +42,39 @@ namespace TP1_IA.model
             return distance;
         }
 
+        public Coordonnees lePlusProche(List<Coordonnees> liste)
+        {
+            int distance = 10000; // infini
+            Coordonnees plusProche = null;
+            foreach (Coordonnees c in liste)
+            {
+                if (distance > this.distance(this._coordonnees, c))
+                {
+                    distance = this.distance(this._coordonnees, c);
+                    plusProche = c;
+                }
+            }
+            return plusProche;
+        }
+
         public void moveAndActToOptimum()
         {
+            
             // chaque mouvement du robot lui fait perdre 1 point
             // les Bijoux et les Poussieres rapportent pas le même nombre de points
             // cherche le bijoux le plus proche et la poussière la plus proche, regarde celui qui raporte le plus de points en incluant les déplacements
             // donne les mouvements et l'actions à faire grâce aux énums sous forme de pile dans Intention.
+
+            // quel mouvement est le plus rentable
+            Coordonnees objectif = null;
+            if (-(distance(_coordonnees, lePlusProche(_connaissances.Bijoux))) + _valeur["bijou"] <
+                    -(distance(_coordonnees, lePlusProche(_connaissances.Poussieres))) + _valeur["poussiere"])
+                objectif = lePlusProche(_connaissances.Bijoux);
+            else
+                objectif = lePlusProche(_connaissances.Poussieres);
+
+            // move vers ce mouvement.
+
         }
     }
 }
