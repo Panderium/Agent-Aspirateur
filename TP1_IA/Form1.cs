0c279b02ce570145229d59e4a7b41a5d895901b5
amotte <nonomotte@gmail.com>
2019-02-09 16:32:48 -0500

    build Arbre

--- TP1_IA/Form1.cs
+++ TP1_IA/Form1.cs
@@ -4,58 +4,84 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP1_IA.model;
 using Enum = TP1_IA.model.Enum;
 
 namespace TP1_IA
 {
     public partial class Form1 : Form
-{
-    Graphics g = null;
-    int numOfCells = 10;
-    int cellSize = 50;
-    
-    
-    
-    public Form1()
     {
-        InitializeComponent();
-        g = Graphics.FromHwnd(Handle);
+        Graphics g = null;
+        int numOfCells = 10;
+        int cellSize = 50;
 
-    }
 
-    private void Form1_Load(object sender, EventArgs e)
-    {
-
-    }
-
-    protected override void OnPaint(PaintEventArgs e)
-    {
-        base.OnPaint(e);
-
-        Brush b = new SolidBrush(Color.White);
-
-        Pen p = new Pen(b, 5f);
-
-        
-        for (int y = 0; y <= numOfCells; ++y)
+        public Form1()
         {
-            g.DrawLine(p, 0, y * cellSize, numOfCells * cellSize, y * cellSize);
+            InitializeComponent();
+            g = Graphics.FromHwnd(Handle);
+
         }
 
-        for (int x = 0; x <= numOfCells; ++x)
+        public void updateUI()
         {
-            g.DrawLine(p, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            this.Invalidate();
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Console.WriteLine("ihihih on draw");
+            base.OnPaint(e);
+
+            Brush b = new SolidBrush(Color.White);
+
+            Pen p = new Pen(b, 5f);
+
+
+            for (int x = 0; x <= numOfCells; ++x)
+            {
+                g.DrawLine(p, 0, x * cellSize, numOfCells * cellSize, x * cellSize);
+
+                g.DrawLine(p, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            }
+
+            Environnement env = Environnement.Instance;
+                
+            drawInCell(p,env.Chambres );
+        }
+
+        private void drawInCell(Pen p, Enum.Chambre[,] chambre)
+
+        {
+            String s = "i";
+            Font f = new Font(FontFamily.GenericMonospace, 20);
+
+            for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+
+            {
+                s = "";
+                switch (chambre[i, j])
+                {
+                    case Enum.Chambre.poussiere:
+                        s = "P";
+                        break;
+                    case Enum.Chambre.bijou:
+                        s = "B";
+                        break;
+                    case Enum.Chambre.poussiereEtBijou:
+                        s = "PB";
+                        break;
+                    
+                }
+
+                g.DrawString(s, f, Brushes.White, i * cellSize + cellSize / 6, j * cellSize + cellSize / 6);
+            }
         }
     }
-
-    private void drawInCell(Pen p, Enum.Chambre chambre)
-    {
-        
-    }
-
-
-}
 }
--- TP1_IA/model/Environnement.cs
+++ TP1_IA/model/Environnement.cs
@@ -9,6 +9,14 @@ namespace TP1_IA.model
     class Environnement
     {
         private Enum.Chambre[ , ] chambres = new Enum.Chambre[10,10];
+        Random rnd = new Random();
+
+        public Enum.Chambre[,] Chambres
+        {
+            get => chambres;
+            set => chambres = value;
+        }
+
         private static Environnement instance=null;
 
 
@@ -17,33 +25,43 @@ namespace TP1_IA.model
             for (int x = 0; x < 10; x++)
                 for (int y = 0; y < 10; y++)
                     chambres[x, y] = Enum.Chambre.vide;
+
+ 
         }
 
         public void generateStuff()
         {
-            Random rnd = new Random();
-            int X = rnd.Next(0, 10); // creates a number between 0 and 9
-            int Y = rnd.Next(0, 10);
-            int typeStuff = rnd.Next(0, 10);
-
-            if (typeStuff < 2)
+            while (true)
             {
-                if (chambres[X, Y] == Enum.Chambre.poussiere)
-                    chambres[X, Y] = Enum.Chambre.poussiereEtBijou;
+                int X = rnd.Next(0, 10); // creates a number between 0 and 9
+                int Y = rnd.Next(0, 10);
 
-                if (chambres[X, Y] == Enum.Chambre.vide)
-                    chambres[X, Y] = Enum.Chambre.bijou;
+                int typeStuff = rnd.Next(0, 10);
+                Console.WriteLine(X);
+                Console.WriteLine(Y);
+                Console.WriteLine(typeStuff);
+                if (typeStuff < 2)
+                {
+                    if (chambres[X, Y] == Enum.Chambre.poussiere)
+                        chambres[X, Y] = Enum.Chambre.poussiereEtBijou;
+
+                    if (chambres[X, Y] == Enum.Chambre.vide)
+                        chambres[X, Y] = Enum.Chambre.bijou;
+
+                }
+                else
+                {
+                    if (chambres[X, Y] == Enum.Chambre.bijou)
+                        chambres[X, Y] = Enum.Chambre.poussiereEtBijou;
+
+                    if (chambres[X, Y] == Enum.Chambre.vide)
+                        chambres[X, Y] = Enum.Chambre.poussiere;
+                }
+
+                Program.updateUI();
+                System.Threading.Thread.Sleep(1000);
                 
             }
-            else
-            {
-                if (chambres[X, Y] == Enum.Chambre.bijou)
-                    chambres[X, Y] = Enum.Chambre.poussiereEtBijou;
-
-                if (chambres[X, Y] == Enum.Chambre.vide)
-                    chambres[X, Y] = Enum.Chambre.poussiere;
-            }
-
         }
 
         public Enum.Chambre getRoom(Coordonnees c)
