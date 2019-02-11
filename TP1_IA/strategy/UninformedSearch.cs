c79d844df084a7df5ae8ec2dea889fc818ac4a99
Panderium <alex.panderium@gmail.com>
2019-02-11 18:10:58 -0500

    Redo uniformed search

--- TP1_IA/strategy/UninformedSearch.cs
+++ TP1_IA/strategy/UninformedSearch.cs
@@ -1,251 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TP1_IA.model;
 
 namespace TP1_IA.strategy
 {
     public class UninformedSearch : SearchStrategy
     {
-        private Node search(Node node, int depth)
+        private List<Node> search(Node curNode, int depth)
         {
-            if (!Desire.desireReach(node) && depth != 0)
+            Queue<Node> file = new Queue<Node>();
+            List<Node> visitedNode = new List<Node>();
+            file.Enqueue(curNode);
+
+
+            while (depth > 0 || Desire.desireReach(curNode))
             {
-                foreach (Node nextNode in node.nextNode())
+                curNode = file.Dequeue();
+                if (!visitedNode.Contains(curNode))
                 {
-                    search(nextNode, depth - 1);
+                    visitedNode.Add(curNode);
+                    foreach (Node nextNode in curNode.nextNode())
+                    {
+                        file.Enqueue(nextNode);
+                        depth--;
+                    }
                 }
             }
 
-            return node;
+            return file.ToList();
         }
 
-        public void execute(Node node, int depth)
+        public List<EnumIA.Action> execute(Node node, int depth)
         {
-            search(node, depth);
+            List<Node> result = search(node, depth);
+            Random rand = new Random();
+            return result.ElementAt(rand.Next(1, result.Count) - 1).getActions();
         }
-
-
-        // recherche en pronfondeur jusqu'à une limite : regarde toutes les actions en partant d'un noeuds jusqu'à la profondeur, puis un autre noeuds etc.
-        public Node depthLimitedSearch(Agent agent, int profondeurMax)
-        {
-            Node root = new Node(agent.Coordonnees, agent.Belief.Jewels, agent.Belief.Dust, 0, 0);
-            Node currentNode = root;
-            int profondeurActuelle = 0;
-            int indexAction = 0;
-            int actionSize = 4; // Quantité d'action dans l'énum
-            Boolean keeprunning = true;
-
-            // tant que l'arbre n'est pas plein
-            while (profondeurActuelle != profondeurMax && indexAction != actionSize && !root.getChild(actionSize - 1).Equals(null) && keeprunning) 
-            {
-                // Si on est pas au fond
-                if(indexAction < actionSize && profondeurActuelle != profondeurMax)
-                {
-                    // on fait l'action choisi et on change le noeud courant
-                    currentNode.addChild(doStuff(currentNode, indexAction));
-                    // Si on a fait toutes les actions pour ce noeud
-                    if(indexAction == actionSize)
-                    {
-                        // si on peut aller en profondeur 
-                        if(profondeurActuelle < profondeurMax)
-                        {
-                            if (root.getChild(indexAction) != null)
-                            {
-                                profondeurActuelle++;
-                                currentNode = root.getChild(indexAction);
-                            }
-                            else
-                            {
-                                if (indexAction < actionSize)
-                                {
-                                    indexAction++;
-                                }
-                                else
-                                {
-                                    profondeurActuelle--;
-                                    indexAction = 0;
-                                }
-                            }
-                        }
-                        // si on est déjà au fond et qu'on a fait toutes les Actions
-                        else
-                        {
-                            
-                            if (root.getFather() != null)
-                            {
-                                profondeurActuelle--;
-                                currentNode = currentNode.getFather();
-                            }
-                            else // fini ou bug
-                            {
-                                keeprunning = false;
-                            }
-                        }
-                        indexAction = 0;
-                    }
-                    // Si il rests des actions à effectuer sur le noeud
-                    else
-                    {
-                        // si on n'est pas au fond
-                        if (profondeurActuelle < profondeurMax)
-                        {
-                            if (root.getChild(indexAction) != null)
-                            {
-                                profondeurActuelle++;
-                                currentNode = root.getChild(indexAction);
-                            }
-                            else
-                            {
-                                if(indexAction < actionSize)
-                                {
-                                    indexAction++;
-                                }
-                                else
-                                {
-                                    profondeurActuelle--;
-                                    indexAction = 0;
-                                }
-                            }
-                        }
-                        // Si on est au fond
-                        else
-                        {
-                            indexAction++;
-                        }
-                    }
-                   
-                }
-                else // c'est qu'on est déja passé par ce coté, il faut avancé dans l'arbre
-                {
-                    if (indexAction == actionSize - 1)
-                    {
-                        if (root.getFather() != null)
-                        {
-                            profondeurActuelle--;
-                            currentNode = currentNode.getFather();
-                        }
-                        else // fini ou bug
-                        {
-                            keeprunning = false;
-                        }
-                    }
-                    else
-                    {
-                        indexAction++;
-
-                    }
-                }
-            }
-            return root;
-
-        }
-
-        public Node doStuff(Node node, int indexAction)
-        {
-            if (indexAction == 0) // haut
-            {
-                if (node.Coordonnees.X == 0)
-                    return null;
-                else
-                    return new Node(new Coordonnees((node.Coordonnees.X - 1), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore()-1, node.getHeuristique());
-            }
-            if (indexAction == 1) // bas
-            {
-                if (node.Coordonnees.X == 10)
-                    return null;
-                else
-                    return new Node(new Coordonnees((node.Coordonnees.X + 1), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore() - 1, node.getHeuristique());
-            }
-            if (indexAction == 2) // droite
-            {
-                if (node.Coordonnees.Y == 10)
-                    return null;
-                else
-                    return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y+1), node.Jewels, node.Dust, node.getScore() - 1, node.getHeuristique());
-            }
-            if (indexAction == 3) // gauche
-            {
-                if (node.Coordonnees.Y == 0)
-                    return null;
-                else
-                    return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y - 1), node.Jewels, node.Dust, node.getScore() - 1, node.getHeuristique());
-            }
-            if (indexAction == 4) // aspirer
-            {
-                if (Environnement.Instance.getRoom(node.Coordonnees).Equals(EnumIA.Chambre.bijou) || Environnement.Instance.getRoom(node.Coordonnees).Equals(EnumIA.Chambre.poussiereEtBijou))
-                    return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore() - 15, node.getHeuristique());
-                else
-                    if(Environnement.Instance.getRoom(node.Coordonnees).Equals(EnumIA.Chambre.poussiere))
-                        return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore() +10, node.getHeuristique());
-                return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore() - 1, node.getHeuristique());
-
-            }
-            if (indexAction == 5) // recuperer
-            {
-                if (Environnement.Instance.getRoom(node.Coordonnees).Equals(EnumIA.Chambre.bijou) || Environnement.Instance.getRoom(node.Coordonnees).Equals(EnumIA.Chambre.poussiereEtBijou))
-                    return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y), node.Jewels, node.Dust, node.getScore() + 20, node.getHeuristique());
-                return new Node(new Coordonnees((node.Coordonnees.X), node.Coordonnees.Y ), node.Jewels, node.Dust, node.getScore() - 1, node.getHeuristique());
-            }
-        }
-
-
-
-
-
-            /* Création d'un arbre
-             * Création profondeur = 0
-             * Création indexAction = 0
-             * while( arbre[profondeurMax].[actionSize] != null)
-             * {
-             *     if(indexAction < actionSize && si arbre[profondeurActuelle].[indexAction] == null){
-             *          ajout dans arbre[profondeurActuelle].[indexAction].environementMAJavecAction
-             *          
-             *          if(indexAction == actionSize-1){
-             *             if(profondeurActuelle < profondeurMax)
-            *                   profondeurActuelle++;
-            *              else
-            *                   profondeur--;
-            *              indexAction = 0;
-            *          } else{
-            *              if(profondeurActuelle < profondeurMax)
-            *                   profondeurActuelle++;
-            *              else{
-            *                   indexAction++;
-            *              }
-            *          }
-        *          }
-             *     else // c'est qu'on est déja passé par ce coté, il faut avancé dans l'arbre
-             *     {
-             *          if(indexAction == actionSize-1)
-             *              profondeur--;
-             *          else
-             *              indexAction++;
-             *     }
-        *          
-             * }
-             */
-        }
-
-        /*  public void recursive(int profondeurActuelle, int profondeurMax, int indexAction)
-          {
-              if(profondeurActuelle == profondeurMax && indexAction == System.Enum.GetNames(typeof(model.Enum.Action)).Length)
-              {
-                  profondeurActuelle--;
-                  indexAction = 0;
-                  recursive(profondeurActuelle, profondeurMax, indexAction);
-              }
-              else
-              {
-                  if()
-              }
-          }*/
     }
-
-/*
- * X = profondeur, Y = Enum.action()
- * jusqu'à profondeur X : 
- * 1 faire l'action Y
- * 2 Si t'es à X, tu remontes ou si Y est au max
- * 3 Si tu peux faire une action Y+1, alors tu la fait
- * 
- */
+}
